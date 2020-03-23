fd75cd9cf12951757456f8a101019b132e7cea3a
Chris Tralie <chris.tralie@gmail.com>
2020-03-22 23:11:40 -0400

    Adding trump tweets since 11/2016

---
+++ Twitter.py
@@ -0,0 +1,7 @@
+import pickle
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+import pandas as pd
+
+MONTHS = {'Jan':'01', 'Feb':'02', 'Mar':'03', 'Apr':'04', 'May':'05', 'Jun':'06', 'Jul':'07', 'Aug':'08', 'Sep':'09', 'Oct':'10', 'Nov':'11', 'Dec':'12'}
